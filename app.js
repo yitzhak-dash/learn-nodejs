d10036ca9df420ae781ec77591111c1fc495d8a1
Yitzhak <yitzhak@razorgrip.com>
2016-11-05 23:26:05 +0200

    Build first module.

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// function statement
-function greet() {
-    console.log('hi');
-}
+var greet = require('./greet');
+
 greet();
-
-// func. are first-class
-function logGreeting(fn) {
-    fn();
-}
-logGreeting(greet);
-
-//function expression
-var greetMe = function () {
-    console.log('Hello body');
-};
-greetMe();
-
-logGreeting(greetMe);
-
-logGreeting(function () {
-    console.log('[*] Hi you');
-});
---
+++ greet.js
@@ -0,0 +1,6 @@
+var greet = function () {
+    console.log('Hello');
+};
+
+// Choose what becomes be available outside of this module for use.
+module.exports = greet;
