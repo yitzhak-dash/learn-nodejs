76d3081209b15fc1970113a7666c058784e149e6
Yitzhak <idashevsky@gmail.com>
2016-11-09 00:01:02 +0200

    inherite from the EventEmitter.

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-var person = {
-    firstName: '',
-    lastName: '',
-    greet: function () {
-        return 'Hello ' + this.firstName + ' ' + this.lastName;
-    }
+var EventEmitter = require('events');
+var util = require('util');
+
+function Greetr() {
+    this.greeting = 'Hello you';
+}
+
+// all object that created from Greetr will be have all props and methods of EventEmitter.
+util.inherits(Greetr, EventEmitter);
+
+Greetr.prototype.greet = function () {
+    console.log('Bill said Monica');
+    this.emit('greet');
 };
 
-// creates new obj. john,
-// the person obj. is prototype of john.
-var john = Object.create(person);
+var greeter = new Greetr();
+greeter.on('greet', function () {
+    console.log("Here's Monica");
+});
 
-
+greeter.greet();
