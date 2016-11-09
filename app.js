9f1d3ce1ac22ba39df8ae8022c13f68de1868d96
Yitzhak <idashevsky@gmail.com>
2016-11-09 20:02:07 +0200

    apply and call nethods

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-var name = "John Doe";
-//template literals
-var greet2 = `Hello ${name}`;
+var obj = {
+    name: "John Doe",
+    greet: function () {
+        console.log(`Hello ${this.name}`);
+    }
+};
+
+obj.greet();
+
+// params in call overrides this keyword.
+obj.greet.call({name: 'Jane Doe'});
+obj.greet.apply({name: 'Donald Duck'});
