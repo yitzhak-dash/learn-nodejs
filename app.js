d6f0680f6e690d3f3b305405fa9b078f9ead7e2d
Yitzhak <idashevsky@gmail.com>
2016-11-09 19:55:28 +0200

    learn template literals

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-var EventEmitter = require('events');
-var util = require('util');
-
-function Greetr() {
-    this.greeting = 'Hello you';
-}
-
-// all object that created from Greetr will be have all props and methods of EventEmitter.
-util.inherits(Greetr, EventEmitter);
-
-Greetr.prototype.greet = function (data) {
-    console.log('Bill said Monica and ' + data);
-    this.emit('greet', data);
-};
-
-var greeter = new Greetr();
-greeter.on('greet', function (data) {
-    console.log("Here's Monica. She's going " + data);
-});
-
-greeter.greet('Zed');
+var name = "John Doe";
+//template literals
+var greet2 = `Hello ${name}`;
