8e7ddbe52146bf26490937fa7aeaff8a9e9b904c
Yitzhak <idashevsky@gmail.com>
2016-11-09 22:13:43 +0200

    learn inheriting from the EventEmitter part 2.

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-var obj = {
-    name: "John Doe",
-    greet: function () {
-        console.log(`Hello ${this.name}`);
-    }
+var EventEmitter = require('events');
+var util = require('util');
+
+function Greetr() {
+    // that will resolve the problem with the inherits
+    EventEmitter.call(this);
+    this.greeting = 'Hello you';
+}
+
+// all object that created from Greetr will be have all props and methods
+//      from prototype of EventEmitter.
+util.inherits(Greetr, EventEmitter);
+
+Greetr.prototype.greet = function () {
+    console.log('Bill said Monica');
+    this.emit('greet');
 };
 
-obj.greet();
+var greeter = new Greetr();
+greeter.on('greet', function () {
+    console.log("Here's Monica");
+});
 
-// params in call overrides this keyword.
-obj.greet.call({name: 'Jane Doe'});
-obj.greet.apply({name: 'Donald Duck'});
+greeter.greet();
